37eb844227c489a60b6f48114effa6363db4e66f
susanzl1115 <susanzl1115@gmail.com>
2019-03-13 18:30:52 -0700

    Add jQuery callback() functions for user story 6

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,37 @@
+var searchCharname = function (e) {
+    e.preventDefault();
+
+    var insurance_identification = $('#identification').val();
+
+    var obj = { 'identification': insurance_identification };
+    console.log(obj);
+    $.ajax({
+        dataType: 'json',
+        type: 'POST',
+        url: '/search_name',
+        data: obj,
+        success: function (data) {
+            var results = data.results;
+            $('#search_result_byname').html('');
+            for (var i = 0; i < results.length; i++) {
+                var str = '<li>' + results[i].provider_name + ' ' + results[i].practitioner_name + '</li>';
+                $('#search_result_byname').append(str);
+            }
+        }
+    });
+};
+
+var render = function () {
+    // renderTable('/practitioners', '#practitioners');
+    //$('#searchbyname').on('click', searchCharname);
+    ('#searchbyname').click();
+};
+
+jQuery(document).ready(function ($) {
+    render();
+});
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
   
   var btns = document.getElementsByClassName("add-btn");
